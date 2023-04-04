f41cfa799d712b84e199f6755841f6686f5eb3cf
Gabriel Fratassi <gabrielfratassi98@gmail.com>
2023-04-04 00:41:01 -0300

    feat: Estrutura projeto com injecao de dependencia configurada

---
+++ Gigacode/Configurations/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Gigacode.Infra.CrossCuting.DependencyInjection;
+
+namespace Gigacode.Services.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServicesInjection(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException();
+
+            NativeDependencyInjection.AddNativeDependencyInjection(services);
+        }
+    }
+}
