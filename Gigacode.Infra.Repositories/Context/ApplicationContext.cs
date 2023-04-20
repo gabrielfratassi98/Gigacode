9f122b8a7662eee1574150709c0518fed439465e
Gabriel Fratassi <gabrielfratassi@evoluaeducacao.com.br>
2023-04-20 20:30:14 -0300

    feat: EntityFramework

---
+++ Gigacode.Infra.Repositories/Context/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gigacode.Infra.Data.Context
+{
+    public class ApplicationContext : DbContext 
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        {
+            
+        }
+
+        protected internal virtual void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+        }
+    }
+}
--- Gigacode/Program.cs
+++ Gigacode/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterServicesInjection();
 
+builder.Services.ConnectionDbSqlServer(builder.Configuration);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
